3fcd8020f2c7d6e145a9e9f9edadf7a7350eec10
ba1l0n <antonio.bailon@gmail.com>
2024-04-03 00:13:59 +0200

    wip ar

--- cuia.py
+++ cuia.py
@@ -131,3 +131,56 @@ class myVideo:
                 if frame is not None:
                     cv2.imshow(titulo, frame)
         cv2.destroyWindow(titulo)
+
+def alphaBlending(fg, bg, x=0, y=0):
+    sfg = fg.shape
+    fgh = sfg[0]
+    fgw = sfg[1]
+
+    sbg = bg.shape
+    bgh = sbg[0]
+    bgw = sbg[1]
+
+    h = max(bgh, y + fgh) - min(0, y)
+    w = max(bgw, x + fgw) - min(0, x)
+
+    CA = np.zeros(shape=(h, w, 3))
+    aA = np.zeros(shape=(h, w))
+    CB = np.zeros(shape=(h, w, 3))
+    aB = np.zeros(shape=(h, w))
+
+    bgx = max(0, -x)
+    bgy = max(0, -y)
+
+    if len(sbg) == 2 or sbg[2]==1:
+        aB[bgy:bgy+bgh, bgx:bgx+bgw] = np.ones(shape=sbg)
+        CB[bgy:bgy+bgh, bgx:bgx+bgw, :] = cv2.cvtColor(bg, cv2.COLOR_GRAY2BGR)
+    elif sbg[2] == 3:
+        aB[bgy:bgy+bgh, bgx:bgx+bgw] = np.ones(shape=sbg[0:2])
+        CB[bgy:bgy+bgh, bgx:bgx+bgw, :] = bg
+    else:
+        aB[bgy:bgy+bgh, bgx:bgx+bgw] = bg[:, :, 3] / 255.0
+        CB[bgy:bgy+bgh, bgx:bgx+bgw, :] = bg[:, :, 0:3]
+
+    fgx = max(0, x)
+    fgy = max(0, y)
+
+    if len(sfg) == 2 or sfg[2]==1:
+        aA[fgy:fgy+fgh, fgx:fgx+fgw] = np.ones(shape=sfg)
+        CA[fgy:fgy+fgh, fgx:fgx+fgw, :] = cv2.cvtColor(fg, cv2.COLOR_GRAY2BGR)
+    elif sfg[2] == 3:
+        aA[fgy:fgy+fgh, fgx:fgx+fgw] = np.ones(shape=sfg[0:2])
+        CA[fgy:fgy+fgh, fgx:fgx+fgw, :] = fg
+    else:
+        aA[fgy:fgy+fgh, fgx:fgx+fgw] = fg[:, :, 3] / 255.0
+        CA[fgy:fgy+fgh, fgx:fgx+fgw, :] = fg[:, :, 0:3]
+
+    aA = cv2.merge((aA, aA, aA))
+    aB = cv2.merge((aB, aB, aB))
+    a0 = aA + aB * (1 - aA)
+    C0 = np.divide(((CA * aA) + (CB * aB)*(1.0 - aA)), a0, out=np.zeros_like(CA), where=(a0!=0))
+
+    res = cv2.cvtColor(np.uint8(C0), cv2.COLOR_BGR2BGRA)
+    res[:, :, 3] = np.uint8(a0[:, :, 0] * 255.0)
+
+    return res
